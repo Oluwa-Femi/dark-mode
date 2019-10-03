89bdad2dc22669b148d5e6ce23d3b8c501a869d3
Oluwa-femi <oluwatolablessing@gmail.com>
2019-10-03 09:20:24 +0100

    First commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,16 @@
 import React, { useState } from 'react';
+import useDarkMode from '../hooks/useDarkMode';
 
 const Navbar = () => {
-  const [darkMode, setDarkMode] = useState(false);
+  
+  // const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useDarkMode('darkMode',false);
+
   const toggleMode = e => {
     e.preventDefault();
     setDarkMode(!darkMode);
   };
+  console.log(darkMode)
   return (
     <nav className="navbar">
       <h1>Crypto Tracker</h1>
@@ -16,7 +21,26 @@ const Navbar = () => {
         />
       </div>
     </nav>
-  );
-};
+    );
+  };
 
-export default Navbar;
+  // const useLocalStorage = (key, initialValue) => {
+//   // we need state
+//   // the initial value needs to be the value in local storage for given key
+//   // and if it's not there, needs to be "initialValue"
+//   let value = JSON.parse(localStorage.getItem(key));
+//   if(!value) value = initialValue;
+//   const [storedValue, setStoredValue] = useState(value);
+
+  
+ //   // let's set the new value
+//   const setValue = value => {
+//     setStoredValue(value); // in state
+//     localStorage.setItem(key, JSON.stringify(value)); // in ls
+//   }; 
+   
+  
+//   return [storedValue, setValue];
+// };
+  
+  export default Navbar;
